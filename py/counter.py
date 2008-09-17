90cb6c9d846fd26af666155b9995cea51bbfff71
Dwight <dmerriman@gmail.com>
2008-09-17 12:26:08 -0400

    fix

--- py/counter.py
+++ py/counter.py
@@ -1,14 +1,9 @@
-n=db.nLoads.findOne() or {'count':0}
-n.count = n.count + 1
-db.nLoads.save(n)
+n=db.nPyPageLoads.findOne() or {'count':0}
+n['count'] = n['count'] + 1
+db.nPyPageLoads.save(n)
 
 print "<html><body>This page has been loaded "
-print n.count
+print n['count']
 print " times. "
 print '<a href="/admin/doc?f=/py/counter.py">[code]</a>'
 print "<p>"
-
-
-
-
-

0ef7bfa3d7f4fbd5f63320946ca6ce9bd27a8190
Dwight <dmerriman@gmail.com>
2008-10-14 14:57:30 -0400

    quicklinks example

--- _init.js
+++ _init.js
@@ -3,6 +3,9 @@
 
 core.user.auth();
 
+/* For quicklinks.jxp */
+db.createCollection('quicklinks', { size: 128 * 1024, capped: true });
+
 /* Function allowed() is called on every request before processing for authentication purposes. 
    The default implementation below denies non-admin users access to anything under /admin/ on the 
    site, but keeps the site completely open otherwise.
